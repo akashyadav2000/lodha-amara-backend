13a61d153612e25e38a1903b07be7f43a6adbb42
akashyadav2000 <125122328+akashyadav2000@users.noreply.github.com>
2025-02-13 23:45:47 +0530

    phone

--- models/Form.js
+++ models/Form.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const formSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  phone: { type: String, required: true },
+  phoneNumber: { type: String, required: true },
+  countryCode: { type: String, required: true },
   email: { type: String },
   popupType: { type: String, required: true }, // 🚨 Required field!
   submittedAt: { type: Date, default: Date.now },
